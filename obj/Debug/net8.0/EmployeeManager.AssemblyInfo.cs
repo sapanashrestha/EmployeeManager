e566c14b52652eafb3e448800aa57e1e3bc95c1a
Sapana Shrestha <shresthasapana01@gmail.com>
2024-04-21 14:12:35 +0545

    implement asynchronous

--- obj/Debug/net8.0/EmployeeManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e96cabfc873ff716f45d2e3344229727b7c52f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d9628ad51cec57d0c1d3024c7faa43da13cc7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
